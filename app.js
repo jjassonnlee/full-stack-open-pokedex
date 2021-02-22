2f234635f674b3b5bc49440f6cc9a6dee20965ba
Jason Lee <jasoncs.lee@innovasolutions.com>
2021-02-22 16:56:00 +0800

    added health check after deploying to prod

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+app.get('/version', (_req, res) => {
+  res.send('1')
+})
 
 app.listen(PORT, () => {
   /* eslint-disable-next-line */
